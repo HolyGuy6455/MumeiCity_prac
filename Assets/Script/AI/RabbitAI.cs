4c471fa2c7d0278c759220c921900f283a9034d6
HolyGuy6455 <sungmin0511a@gmail.com>
2021-12-05 23:27:05 +0900

    토끼AI

---
+++ Assets/Script/AI/RabbitAI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitAI : MonoBehaviour
+{
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Rigidbody rigidbody;
+    [SerializeField]
+    Animator animator;
+    public float noticeDistance = 3.0f;
+    public float jumpPower = 3.0f;
+    bool isNoticed = false;
+    int groundLayerMask;
+
+    void Start()
+    {
+        groundLayerMask = (1 << LayerMask.NameToLayer("Ground")) + (1 << LayerMask.NameToLayer("Building"));
+    }
+
+    void Update()
+    {
+        if(!isNoticed){
+            float distanceToPLayer = Vector3.Distance(this.transform.position,playerTransform.position);
+            if(distanceToPLayer < noticeDistance){
+                animator.SetTrigger("Notice");
+                animator.SetBool("isNoticed",true);
+                isNoticed = true;
+            }
+        }
+        animator.SetBool("isGrounded",IsGrounded());
+    }
+
+    public void Jump(){
+        Vector3 movement = (this.transform.position-playerTransform.position).normalized;
+        this.rigidbody.AddForce(new Vector3(movement.x,1,movement.z)*jumpPower,ForceMode.Impulse);
+    }
+
+    public bool IsGrounded() {
+        return Physics.Raycast(this.transform.position, Vector3.down, 1.0f, groundLayerMask);
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(this.transform.position, noticeDistance);
+    }
+}
--- Assets/Script/ItemAndEntity/Interactable.cs
+++ Assets/Script/ItemAndEntity/Interactable.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
 
-    public float radius = 3f;
+    // public float radius = 3f;
     public bool isInteractable = false;
     public Transform player;
 
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
-    }
+    // private void OnDrawGizmosSelected() {
+    //     Gizmos.color = Color.yellow;
+    //     Gizmos.DrawWireSphere(transform.position, radius);
+    // }
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +21,20 @@ public class Interactable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector2.Distance(player.position, transform.position);
-        if(!isInteractable && (distance < radius) ){
-            Debug.Log(gameObject.name + "Interactable");
-            isInteractable = true;
-            GameManager.Instance.AddInteractable(this);
-        }else if(isInteractable && (distance > radius)){
-            Debug.Log(gameObject.name + "no more Interactable");
-            isInteractable = false;
-            GameManager.Instance.RemoveInteractable(this);
-        }
+        // float distance = Vector2.Distance(player.position, transform.position);
+        // if(!isInteractable && (distance < radius) ){
+        //     Debug.Log(gameObject.name + "Interactable");
+        //     isInteractable = true;
+        //     GameManager.Instance.AddInteractable(this);
+        // }else if(isInteractable && (distance > radius)){
+        //     Debug.Log(gameObject.name + "no more Interactable");
+        //     isInteractable = false;
+        //     GameManager.Instance.RemoveInteractable(this);
+        // }
 
-        if(player == null){
-            player = GameManager.Instance.PlayerTransform;
-        }
+        // if(player == null){
+        //     player = GameManager.Instance.PlayerTransform;
+        // }
     }
 
     private void OnDestroy() {
