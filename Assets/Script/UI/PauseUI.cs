3ae75dc0c2ff7ffeb94dca12a94cd93ff13b1b5c
HolyGuy6455 <sungmin0511a@gmail.com>
2021-12-08 19:32:21 +0900

    저장과 불러오기 - 아이템(땅에 떨어져있는거)

--- Assets/Script/UI/PauseUI.cs
+++ Assets/Script/UI/PauseUI.cs
@@ -6,8 +6,10 @@ using System.IO;
 public class PauseUI : MonoBehaviour{
     [TextArea] public string savefile;
     [SerializeField] string _fileName = "01.save";
+    [SerializeField] GameObject itemPickupPrefab;
     class SaveForm {
         public List<BuildingData> buildingDatas = new List<BuildingData>();
+        public List<ItemData> itemPickupDatas = new List<ItemData>();
     }
 
 
@@ -18,6 +20,15 @@ public class PauseUI : MonoBehaviour{
         foreach (Transform childTransform in buildingsParent.transform){
             BuildingData buildingData = childTransform.GetComponent<BuildingObject>().buildingData;
             saveForm.buildingDatas.Add(buildingData);
+        }
+
+        GameObject itemPickupParent = GameManager.Instance.itemPickupParent;
+        foreach (Transform childTransform in itemPickupParent.transform){
+            ItemData itemData = childTransform.GetComponent<ItemPickup>().item;
+            itemData.positionX = childTransform.position.x;
+            itemData.positionY = childTransform.position.y;
+            itemData.positionZ = childTransform.position.z;
+            saveForm.itemPickupDatas.Add(itemData);
         }
         savefile = JsonUtility.ToJson(saveForm,true);
 
@@ -61,6 +72,40 @@ public class PauseUI : MonoBehaviour{
         }
         // string에서 데이터를 읽어들임
         SaveForm saveForm = JsonUtility.FromJson<SaveForm>(savefile);
-        GameManager.Instance.buildingManager.LoadBuilding(saveForm.buildingDatas);
+
+        foreach (BuildingData data in saveForm.buildingDatas){
+            // 데이터로부터 프리셋과 위치를 읽어온다
+            BuildingPreset buildingPreset = data.buildingPreset;
+            Vector3 location = new Vector3();
+            location.x = data.positionX;
+            location.y = data.positionY;
+            location.z = data.positionZ;
+            // 건물을 생성하고 데이터로 초기화한다
+            GameObject Built =  Instantiate(GameManager.Instance.buildingManager.building,location,Quaternion.identity);
+            BuildingObject BuiltObject = Built.GetComponent<BuildingObject>();
+            Built.transform.SetParent(buildingsParent.transform);
+            BuiltObject.Initialize(data);
+        }
+
+        // 맵에 남아있는 아이템을 전부 없앤다
+        foreach (Transform childTransform in GameManager.Instance.itemPickupParent.transform){
+            Destroy(childTransform.gameObject);
+        }
+
+        foreach (ItemData data in saveForm.itemPickupDatas){
+            // 데이터로부터 위치를 읽어온다
+            Vector3 location = new Vector3();
+            location.x = data.positionX;
+            location.y = data.positionY;
+            location.z = data.positionZ;
+            // 아이템 데이터로 픽업아이템을 생성한다
+            GameObject itemObject = Instantiate(itemPickupPrefab,location,Quaternion.identity);
+            ItemPickup itemPickup = itemObject.GetComponent<ItemPickup>();
+            itemPickup.item = data;
+            itemPickup.IconSpriteUpdate();
+            itemPickup.GetComponent<Rigidbody>().AddForce(new Vector3(0,7,0),ForceMode.Impulse);
+            itemObject.transform.SetParent(GameManager.Instance.itemPickupParent.transform);
+        }
+
     }
 }
