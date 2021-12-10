da21d17f4b083145ea7fb154acfa73f0c6d45ec4
HolyGuy6455 <sungmin0511a@gmail.com>
2021-12-10 11:05:23 +0900

    예정에 없던 줌인&줌아웃 기능

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
@@ -37,6 +38,13 @@ public class GameManager : MonoBehaviour
     }
     public GameTab presentGameTab;
     private GameTab pastGameTab;
+
+    [SerializeField] float zoom = 6.0f;
+    [SerializeField] float zoomMin = 4.0f;
+    [SerializeField] float zoomMax = 12.0f;
+    [SerializeField] float zoomSpeed = 1.0f;
+    [SerializeField] float zoomAmount = 1.0f;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
 
     public static GameManager Instance{
         get{
@@ -88,6 +96,10 @@ public class GameManager : MonoBehaviour
     }
 
     private void Update() {
+        zoom -= Input.GetAxisRaw("Mouse ScrollWheel")*zoomAmount;
+        zoom = (zoom<zoomMin)?zoomMin:(zoom>zoomMax)?zoomMax:zoom;
+        virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, zoom, Time.deltaTime*zoomSpeed);
+        
         if(Input.GetButtonDown("Inventory")){
             if(presentGameTab != GameTab.ITEM){
                 presentGameTab = GameTab.ITEM;
