94ca40cf2a31803c23fe078db1e7c95c75124675
HolyGuy6455 <sungmin0511a@gmail.com>
2022-01-01 06:19:25 +0900

    건물 UI가 왜 안뜨나 했네....간단한 버그 픽스?

--- Assets/Script/UI/TaskUIBundle.cs
+++ Assets/Script/UI/TaskUIBundle.cs
@@ -6,11 +6,21 @@ public class TaskUIBundle : MonoBehaviour{
     [SerializeField] InventoryUI inventoryUI;
     [SerializeField] ForesterUI foresterUI;
     [SerializeField] TentUI tentUI;
+    // [SerializeField] MineUI mineUI;
+    [SerializeField] ForesterUI mineUI;         // 임시용
+    [SerializeField] ForesterUI foodStorageUI; // 임시용
+
     public void LoadInventoryUIData(){
         inventoryUI.LoadItemSlotData();
     }
     public void LoadForesterUIData(){
         foresterUI.LoadItemSlotData();
+    }
+    public void LoadMineUIData(){
+        mineUI.LoadItemSlotData();
+    }
+    public void LoadFoodStorageUIData(){
+        foodStorageUI.LoadItemSlotData();
     }
     public void LoadTentUIData(){
         tentUI.LoadItemSlotData();
