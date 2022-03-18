44b8ca8cbbe290921dc915f5550a6ef251107dbc
HolyGuy6455 <sungmin0511a@gmail.com>
2022-03-19 05:13:42 +0900

    친구 추가

---
+++ Assets/Script/AI/Friend.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Pathfinding;
+
+public class Friend : MonoBehaviour{
+    [SerializeField] AIDestinationSetter aIDestination; 
+    [SerializeField] GameObject targetObject;
+    [SerializeField] Animator freindAnimator;
+
+    private void Update() {
+        float distance = Vector3.Distance(this.transform.position, targetObject.transform.position);
+        Vector3 direction = (targetObject.transform.position - this.transform.position).normalized;
+        
+        freindAnimator.SetFloat("MoveX",direction.x);
+        freindAnimator.SetFloat("MoveY",direction.z);
+        freindAnimator.SetFloat("Distance",distance);
+
+        if(distance > 3.0f){
+            aIDestination.target = targetObject.transform;
+        }else{
+            aIDestination.target = null;
+        }
+        Invoke("Spin",30.0f);
+    }
+
+    private void Spin(){
+        freindAnimator.SetTrigger("Spin");
+    }
+
+}
